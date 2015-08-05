035319cd529622ee91214b1aa3c1b65d36103b84
kwdoyle <cheeze13579@gmail.com>
2015-08-05 01:11:49 -0400

    Added dates to experiment cultures

--- Exp Cultures.R
+++ Exp Cultures.R
@@ -49,78 +49,109 @@ colnames(exp13) <- c("time", "1nmFe", "100nmFe", "1nmDiameter", "100nmDiameter")
 colnames(exp14) <- c("time", "1nmFe", "100nmFe", "1nmDiameter", "100nmDiameter")
 colnames(exp15) <- c("time", "1nmFe", "100nmFe", "1nmDiameter", "100nmDiameter")
 
+
 #plot them
 plot(exp1$time, log(exp1$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
      main="Experiment Culture # 1", type="b", col="red")
 points(exp1$time, log(exp1$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
 
+
 plot(exp2$time, log(exp2$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
      main="Experiment Culture # 2", type="b", col="red")
 points(exp2$time, log(exp2$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
+
 
 plot(exp3$time, log(exp3$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
      main="Experiment Culture # 3", type="b", col="red")
 points(exp3$time, log(exp3$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
 
+
 plot(exp4$time, log(exp4$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
      main="Experiment Culture # 4", type="b", col="red", ylim=c(7,12))
 points(exp4$time, log(exp4$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
+
 
 plot(exp5$time, log(exp5$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
      main="Experiment Culture # 5", type="b", col="red")
 points(exp5$time, log(exp5$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
 
+
 plot(exp6$time, log(exp6$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
      main="Experiment Culture # 6", type="b", col="red")
 points(exp6$time, log(exp6$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
+
 
 plot(exp7$time, log(exp7$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
      main="Experiment Culture # 7", type="b", col="red")
 points(exp7$time, log(exp7$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
 
-plot(exp8$time, log(exp8$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
-     main="Experiment Culture # 8", type="b", col="red")
-points(exp8$time, log(exp8$"1nmFe"), type="b", col="blue")
-legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
 
+## Second Run
+
+# 5/28
 plot(exp9$time, log(exp9$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
-     main="Experiment Culture # 9", type="b", col="red")
+     main="Experiment Culture # 9", type="b", col="red",
+     sub="used on 5/28")
 points(exp9$time, log(exp9$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
 
+
+# 5/29
 plot(exp10$time, log(exp10$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
-     main="Experiment Culture # 10", type="b", col="red")
+     main="Experiment Culture # 10", type="b", col="red",
+     sub="used on 5/29")
 points(exp10$time, log(exp10$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
 
+
+# 5/30
 plot(exp11$time, log(exp11$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
-     main="Experiment Culture # 11", type="b", col="red")
+     main="Experiment Culture # 11", type="b", col="red",
+     sub="used on 5/30")
 points(exp11$time, log(exp11$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
 
+
+# 5/31
+plot(exp8$time, log(exp8$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
+     main="Experiment Culture # 8", type="b", col="red",
+     sub="used on 5/31")
+points(exp8$time, log(exp8$"1nmFe"), type="b", col="blue")
+legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
+
+
+# 6/1
 plot(exp12$time, log(exp12$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
-     main="Experiment Culture # 12", type="b", col="red")
+     main="Experiment Culture # 12", type="b", col="red",
+     sub="used on 6/1")
 points(exp12$time, log(exp12$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
+
 
 plot(exp13$time, log(exp13$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
      main="Experiment Culture # 13", type="b", col="red")
 points(exp13$time, log(exp13$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
 
+
+# 6/3
 plot(exp14$time, log(exp14$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
-     main="Experiment Culture # 14", type="b", col="red")
+     main="Experiment Culture # 14", type="b", col="red",
+     sub="used on 6/3")
 points(exp14$time, log(exp14$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
 
+
+# 6/4
 plot(exp15$time, log(exp15$"100nmFe"), xlab="time (hours)", ylab="log(cells / mL)",
-     main="Experiment Culture # 15", type="b", col="red")
+     main="Experiment Culture # 15", type="b", col="red",
+     sub="used on 6/4")
 points(exp15$time, log(exp15$"1nmFe"), type="b", col="blue")
 legend("topleft", c("1 nM Fe", "100 nM Fe"), col=c("blue", "red"), lwd=2)
